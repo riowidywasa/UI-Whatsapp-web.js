4838627020878c1ff00aca15cb77aaf0ce1491c0
riowidywasa <riowidywasa@gmail.com>
2023-11-15 10:02:02 +0700

    fix rensponse
    Signed-off-by: riowidywasa <riowidywasa@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -8,16 +8,24 @@ const socket = io.connect("http://localhost:3002", {})
 export default function Home() {
   const [session, setSession] = useState("")
   const [qrCode, setQrCode] = useState("")
+  const [message, setMessage] = useState("")
   const createSessionForWhaatsapp = () => {
     // ganti session menjadi name dari admin
-    socket.emit("createSession",{id:"admin"})
+    socket.emit("createSession", { id: "admin" })
   }
+
+
+
   useEffect(() => {
     socket.emit("connected", "hello from client");
-    
+    socket.on("message", (msg) => {
+      console.log(msg);
+      setMessage(msg)
+    })
+
     socket.on("qr", (data) => {
       console.log(data);
-      const {qr} = data
+      const { qr } = data
       console.log("QR RECEIVED", qr);
       setQrCode(qr);
     })
@@ -33,14 +41,16 @@ export default function Home() {
         <h1>QR CODE</h1>
 
         <div style={{ marginBottom: "40px" }}>
-
+        <h1>Test {message}</h1>
           <input type='text' value={session} onChange={(e) => {
             setSession(e.target.value)
           }} />
           <br /> <br />
           <button onClick={createSessionForWhaatsapp}>Create Session</button>
           <br /> <br />
-          <QRCode  value={qrCode}></QRCode>
+        
+          <br /> <br />
+          <QRCode value={qrCode}></QRCode>
         </div>
       </div>
     </main>
